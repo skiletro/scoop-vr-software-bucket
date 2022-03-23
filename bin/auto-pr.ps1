e027e3141330cf404ce407e38fd049d9cf2091eb
Sourse Corst <31012735+soursecorst@users.noreply.github.com>
2022-03-23 21:53:36 +0300

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "soursecorst/scoop-vr:main"
+    [String]$upstream = "soursecorst/vr:main"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
